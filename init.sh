63baa055542d92ecb8e9741fb31880f0af4fc207
Andrew Wong <andrew.j.wong@outlook.com>
2020-08-19 23:13:39 +1000

    Fix update.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-
 cd "$(dirname "$0")"
 
+# Update repo and fetch submodules
+git pull
 git submodule update --init
 
+# Generate updater
 cat > ./update.sh << :EOF
 #!/bin/sh
 
-cd '\$(dirname "\$0")'
-git submodule foreach git pull origin master
+cd "\$(dirname "\$0")"
+git pull
+git submodule foreach git pull
 :EOF
 chmod a+x ./update.sh
 
+# Wow magical disappearing file!
+rm "$0"
